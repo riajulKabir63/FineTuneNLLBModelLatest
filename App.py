9f4bfe2059617f333400a1400db3d5b27d03d17a
Riajul Kabir <ichkabir@gmail.com>
2024-03-01 22:29:02 +0600

    app commit

---
+++ App.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+import numpy as np
+from transformers import NllbTokenizer, AutoModelForSeq2SeqLM
+import torch
+
+@st.cache_resource
+def get_model():
+    tokenizer = NllbTokenizer.from_pretrained('facebook/nllb-200-distilled-600M')
+    model = AutoModelForSeq2SeqLM.from_pretrained("riajul/FineTunedNLLBModelLatest")
+    return tokenizer,model
+
+
+tokenizer,model = get_model()
+st.title('English to Rohingya Translator App')
+user_input = st.text_area('Enter Text to translate')
+button = st.button("Translate")
+
+if user_input and button :
+    inputs = tokenizer(user_input, return_tensors="pt", truncation=True, padding=True)
+    output = model.generate(**inputs)
+    translated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+    st.write("Translated Rohingya text: ", translated_text)
